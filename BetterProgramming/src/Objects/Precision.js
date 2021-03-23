9fdbc5dbb5651d592ce550092ea834aa3eac16ee
zcc <listenzcc@mail.bnu.edu.cn>
2021-03-23 17:41:34 +0800

    Improve BetterProgramming

---
+++ BetterProgramming/src/Objects/Precision.js
@@ -0,0 +1,15 @@
+/*
+File: Precision.js
+Aim: Example of a number
+*/
+
+// The number of 100 / 3
+const a = 100 / 3;
+
+// The int precision
+// 33
+console.log(parseInt(a));
+
+// The float precision
+// 33.333333333333336
+console.log(parseFloat(a));
---
+++ BetterProgramming/src/Objects/Property.js
@@ -0,0 +1,31 @@
+/*
+File: Property.js
+Aim: Example of Property
+*/
+
+// We think the obj is the collection of scores
+
+// The raw scores
+let obj = [79, 54, 80, 90];
+
+// The scoring thresholds
+obj.thresholds = {
+    A: 90,
+    B: 80,
+    C: 70,
+    D: 60,
+    E: 0,
+};
+
+// Scoring the raw scores
+obj.score = (e, i, t) => {
+    for (let s in t.thresholds) {
+        if (e >= t.thresholds[s]) {
+            return [i, s];
+        }
+    }
+};
+
+// See what we got
+// [ [ 0, 'C' ], [ 1, 'E' ], [ 2, 'B' ], [ 3, 'A' ] ]
+console.log(obj.map(obj.score));
